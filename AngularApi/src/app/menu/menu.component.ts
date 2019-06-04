062aedf75a4bfe71161d37d629f2e7ab327f96a7
renatachagasc <rehcardosochagas@gmail.com>
2019-06-03 21:55:54 -0300

    subindo projeto

---
+++ AngularApi/src/app/menu/menu.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  public texto: string
+  constructor() { }
+
+  ngOnInit() {
+    this.texto = "Olá, tudo bem?"
+  }
+  clicou(){
+    this.texto = "Clicou"
+  }
+
+}
